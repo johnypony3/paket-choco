9f8513759dc15c208699cc1d5d2ca139e81e7a5f
johnypony3 <edward@theemm.com>
2017-03-02 23:51:31 -0800

    when using fix-notation, versions no longer match, outputting to a packaged file `.version`

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = 'Stop';
 
-$ogversion = Get-Content .version
+$versionPath = Join-Path -Path $PSScriptRoot -ChildPath .version
+$ogversion = Get-Content $versionPath
+
 $paketInfo = Invoke-RestMethod -Uri 'https://api.github.com/repos/fsprojects/Paket/releases'
 $repoInfo = $paketInfo | where { $_.tag_name -eq $ogversion }
 
