d2ef83a17f51c96ac688b5dcaf86873eba26fcfd
ohnypony3 <edward@theemm.com>
2017-04-21 02:18:47 -0700

    when using fix-notation, versions no longer match, outputting to a packaged file `.version`
    removed skip
    
    Update generateAllPackages.ps1
    
    actually skip existing packages
    
    fixes as per chocolatey moderators; updated package author to what is in the original repo; added the VERIFICATION.txt file; updated the packate to not download assets rather to package them;
    
    added package exist check override
    
    added VERIFICATION.txt file as per moderators
    
    updated the VERIFICATION.txt to the web edition of the releases page instead of the api
    
    updated the VERIFICATION.txt file as per moderators to contain the url of the release
    
    actually downloading the assets
    
    Merge pull request #2 from johnypony3/feature/fixing-chocorequests
    
    fixes as per chocolatey moderators
    modified the version file and appveyor to save it
    
    Merge pull request #3 from johnypony3/feature/adding-buildinfo
    
    modified the version file and appveyor to save it

--- powershell-helpers/generateAllPackages.ps1
+++ powershell-helpers/generateAllPackages.ps1
@@ -1,14 +1,64 @@
-﻿$paketRepo = 'https://api.github.com/repos/johnypony3/Paket'
+$paketRepo = 'https://github.com/fsprojects/Paket'
 $paketInfos = Invoke-RestMethod -Uri 'https://api.github.com/repos/fsprojects/Paket/releases'
 $paketRepoInfo = Invoke-RestMethod -Uri $paketRepo
+
 $packageOutputPath = Join-Path -Path $PSScriptRoot -ChildPath 'packages'
 mkdir $packageOutputPath
 
+$packagePayloadPath = Join-Path -Path $PSScriptRoot -ChildPath '..\payload'
+mkdir $packagePayloadPath
+
 $nuspecTemplatePath = Join-Path -Path $PSScriptRoot -ChildPath paket.template.nuspec
+$verificationTemplatePath = Join-Path -Path $PSScriptRoot -ChildPath VERIFICATION.template.txt
+
 $nuspecPath = Join-Path -Path $PSScriptRoot -ChildPath paket.nuspec
+$verificationPath = Join-Path -Path $PSScriptRoot -ChildPath ..\tools\VERIFICATION.txt
+
+$versionPath = Join-Path -Path $PSScriptRoot -ChildPath .version
 $assetPath = Join-Path -Path $PSScriptRoot -ChildPath payload
 
 choco apiKey -k $ENV:CHOCO_KEY -source https://chocolatey.org/
+
+function BuildInfoFileGenerator {
+  param([string]$ogVersion)
+
+  $hash = @{}
+  $hash.Add("APPVEYOR", $ENV:APPVEYOR)
+  $hash.Add("CI", $ENV:CI)
+  $hash.Add("APPVEYOR_API_URL", $ENV:APPVEYOR_API_URL)
+  $hash.Add("APPVEYOR_ACCOUNT_NAME", $ENV:APPVEYOR_ACCOUNT_NAME)
+  $hash.Add("APPVEYOR_PROJECT_ID", $ENV:APPVEYOR_PROJECT_ID)
+  $hash.Add("APPVEYOR_PROJECT_NAME", $ENV:APPVEYOR_PROJECT_NAME)
+  $hash.Add("APPVEYOR_PROJECT_SLUG", $ENV:APPVEYOR_PROJECT_SLUG)
+  $hash.Add("APPVEYOR_BUILD_FOLDER", $ENV:APPVEYOR_BUILD_FOLDER)
+  $hash.Add("APPVEYOR_BUILD_ID", $ENV:APPVEYOR_BUILD_ID)
+  $hash.Add("APPVEYOR_BUILD_NUMBER", $ENV:APPVEYOR_BUILD_NUMBER)
+  $hash.Add("APPVEYOR_BUILD_VERSION", $ENV:APPVEYOR_BUILD_VERSION)
+  $hash.Add("APPVEYOR_BUILD_WORKER_IMAGE", $ENV:APPVEYOR_BUILD_WORKER_IMAGE)
+  $hash.Add("APPVEYOR_PULL_REQUEST_NUMBER", $ENV:APPVEYOR_PULL_REQUEST_NUMBER)
+  $hash.Add("APPVEYOR_PULL_REQUEST_TITLE", $ENV:APPVEYOR_PULL_REQUEST_TITLE)
+  $hash.Add("APPVEYOR_JOB_ID", $ENV:APPVEYOR_JOB_ID)
+  $hash.Add("APPVEYOR_JOB_NAME", $ENV:APPVEYOR_JOB_NAME)
+  $hash.Add("APPVEYOR_JOB_NUMBER", $ENV:APPVEYOR_JOB_NUMBER)
+  $hash.Add("APPVEYOR_REPO_PROVIDER", $ENV:APPVEYOR_REPO_PROVIDER)
+  $hash.Add("APPVEYOR_REPO_SCM", $ENV:APPVEYOR_REPO_SCM)
+  $hash.Add("APPVEYOR_REPO_NAME", $ENV:APPVEYOR_REPO_NAME)
+  $hash.Add("APPVEYOR_REPO_BRANCH", $ENV:APPVEYOR_REPO_BRANCH)
+  $hash.Add("APPVEYOR_REPO_TAG", $ENV:APPVEYOR_REPO_TAG)
+  $hash.Add("APPVEYOR_REPO_TAG_NAME", $ENV:APPVEYOR_REPO_TAG_NAME)
+  $hash.Add("APPVEYOR_REPO_COMMIT", $ENV:APPVEYOR_REPO_COMMIT)
+  $hash.Add("APPVEYOR_REPO_COMMIT_AUTHOR", $ENV:APPVEYOR_REPO_COMMIT_AUTHOR)
+  $hash.Add("APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL", $ENV:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL)
+  $hash.Add("APPVEYOR_REPO_COMMIT_TIMESTAMP", $ENV:APPVEYOR_REPO_COMMIT_TIMESTAMP)
+  $hash.Add("APPVEYOR_REPO_COMMIT_MESSAGE", $ENV:APPVEYOR_REPO_COMMIT_MESSAGE)
+  $hash.Add("APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED", $ENV:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED)
+  $hash.Add("APPVEYOR_SCHEDULED_BUILD", $ENV:APPVEYOR_SCHEDULED_BUILD)
+  $hash.Add("PLATFORM", $ENV:PLATFORM)
+  $hash.Add("VERSION", $ENV:PLATFORM)
+  $hash.Add("CONFIGURATION", $ogVersion)
+
+  $hash | ConvertTo-Json | Out-File $versionPath
+}
 
 function CheckIfUploadedToChoco {
   param([string]$chocoUrl)
@@ -26,16 +76,23 @@ function CheckIfUploadedToChoco {
 }
 
 $paketInfos | % {
+    $ogversion = $_.tag_name
+    $downloadUrl = $_.html_url
+
     $skip = $false
-    #$skip = $_.tag_name -like '*beta*'
-    #$skip = $skip -or $_.tag_name -like '*3.36.0*'
+    $skip = !$ogversion
+
+    $overrideExistingPackageCheck = $true
+
+    #$skip = $ogversion -notlike '*beta*'
+    $skip = $skip -or $ogversion -notlike '*4.0.7*'
 
     if ($skip) {
-      Write-Host "skipping version:"$_.tag_name
+      Write-Host "skipping version:"$ogversion
       return
     }
 
-    $version = $_.tag_name #-replace '-', '.022620174-'
+    $version = $ogversion# -replace '-', '.03032017-'
     Write-Host "working on version:"$version
 
     $packageName = "Paket.$version.nupkg"
@@ -45,15 +102,28 @@ $paketInfos | % {
     Write-Host $chocoUrl
 
     if (CheckIfUploadedToChoco -chocoUrl $chocoUrl) {
-      Write-Host "package exists, skipping:"$packageName
-      return;
+      if (!($overrideExistingPackageCheck)){
+        Write-Host "package exists, skipping:"$packageName
+        return;
+      }
+
+      Write-Host "package exists, continuing:"$packageName
     } else {
       Write-Host "package does not exist:"$packageName
     }
 
+    Remove-Item "$packagePayloadPath/*" -recurse
+    $repoInfo = $paketInfos | where { $_.tag_name -eq $ogversion }
+
+    $repoInfo.assets | % {
+        $fileNameFull = Join-Path -Path $packagePayloadPath -ChildPath $_.name
+        Invoke-WebRequest -OutFile $fileNameFull -Uri $_.browser_download_url
+        Write-Host "  -> downloaded $_.name"
+    }
+
     [xml]$nuspec = Get-Content $nuspecTemplatePath
     $nuspec.package.metadata.id = 'paket'
-    $nuspec.package.metadata.title = 'paket'
+    $nuspec.package.metadata.title = 'Paket'
     $nuspec.package.metadata.version = $version
     $nuspec.package.metadata.authors = $paketRepoInfo.owner.login
     $nuspec.package.metadata.projectUrl = $paketRepoInfo.homepage
@@ -64,8 +134,11 @@ $paketInfos | % {
     $nuspec.package.metadata.mailingListUrl = $paketRepo
     $nuspec.package.metadata.bugTrackerUrl = $paketRepo
     $nuspec.package.metadata.packageSourceUrl = $paketRepo
-
     $nuspec.Save($nuspecPath)
+
+    Copy-Item $verificationTemplatePath $verificationPath
+    Add-Content $verificationPath "The download url for this packages release is <$downloadUrl>"
+    BuildInfoFileGenerator $ogversion
 
     choco pack $nuspecPath --outputdirectory $packageOutputPath
 }
--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,14 +1,3 @@
 $ErrorActionPreference = 'Stop';
 
-$paketInfo = Invoke-RestMethod -Uri 'https://api.github.com/repos/fsprojects/Paket/releases'
-$repoInfo = $paketInfo | where { $_.tag_name -eq $ENV:chocolateyPackageVersion }
-
-$toolsDir = Join-Path -Path $ENV:chocolateyPackageFolder -ChildPath 'tools'
-
-$repoInfo.assets | % {
-    $fileNameFull = Join-Path -Path $toolsDir -ChildPath $_.name
-    Get-ChocolateyWebFile -PackageName $ENV:chocolateyPackageName -FileFullPath $fileNameFull -Url $_.browser_download_url
-}
-
-
 Install-ChocolateyEnvironmentVariable -VariableName "PaketExePath" -VariableValue "paket.exe" -VariableType Machine
